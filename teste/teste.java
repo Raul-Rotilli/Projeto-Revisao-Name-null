c776ee13abe495dffe4f1b8ae5995403e1b102e7
Rafael Rodrigues Moraes <135690957+RafaRoMoraes@users.noreply.github.com>
2023-10-31 00:21:47 -0300

    teste RRM

--- teste/teste.java
+++ teste/teste.java
@@ -4,4 +4,5 @@ public class teste {
     public static void main(String[] args) {
         System.out.println("Oi");
     }
+
 }
---
+++ teste/teste_rafa.java
@@ -0,0 +1,7 @@
+package teste;
+
+public class teste_rafa {
+    public static void main(String args[]){
+        System.out.println("Fala lindxs");
+    }
+}
