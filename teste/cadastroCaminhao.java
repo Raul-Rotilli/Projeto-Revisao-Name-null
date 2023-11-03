708434856e9128d99bd26c6db365de1233d44e8e
Rafael Rodrigues Moraes <135690957+RafaRoMoraes@users.noreply.github.com>
2023-11-03 15:22:37 -0300

    Alterações iniciais-Cadastro caminhão

---
+++ teste/cadastroCaminhao.java
@@ -0,0 +1,38 @@
+package teste;
+
+public class cadastroCaminhao {
+        private int Tipo;
+        private double valorKm, capacidade, capacidadeTotal;
+
+        public cadastroCaminhao(int tipo){
+            this.Tipo = tipo;
+
+            this.capacidade = 0;
+
+            if(this.Tipo == 1){
+                this.capacidadeTotal = 1000.0;
+                this.valorKm = 5.83;
+            }else if(this.Tipo == 2){
+                this.capacidadeTotal = 4000.0;
+                this.valorKm = 13.42;
+            }else if(this.Tipo == 3){
+                this.capacidadeTotal = 10000.0;
+                this.valorKm = 29.21;
+            }else{
+                System.out.println("Erro!!! O custo não consegue ser calculado por conta de um código inválido!!!");
+            }
+        }
+
+        public double Calculo(double distancia, int quantCaminhoes){
+            double Valor = this.valorKm * distancia * quantCaminhoes;
+            return Valor;
+        }
+
+        public boolean Peso(double peso){
+            if(peso > capacidadeTotal || peso < capacidade){
+                return false;
+            }else{
+                return true;
+            }
+        }
+}
--- teste/teste_rafa.java
+++
@@ -1,7 +0,0 @@
-package teste;
-
-public class teste_rafa {
-    public static void main(String args[]){
-        System.out.println("Fala lindxs");
-    }
-}
