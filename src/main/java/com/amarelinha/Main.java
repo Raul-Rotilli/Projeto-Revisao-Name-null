6cc98b28ad438cd17cea6fdd746f20c96e7c6547
Raul Rotilli Aguirre <raulrotilli1@gmail.com>
2023-11-06 15:38:36 -0300

    atualizando a main

--- src/main/java/com/amarelinha/Main.java
+++ src/main/java/com/amarelinha/Main.java
@@ -29,7 +29,57 @@ public class Main {
 
             switch (opcao){
                 case 1:
+                    lc.listarCidades();
 
+                    s.nextLine();
+                    System.out.println("Escolha as cidades:");
+                    System.out.println("");
+
+                    System.out.println("Digte a cidade de partida:");
+                    String cidade1 = s.nextLine().toUpperCase();
+
+                    System.out.println("Digte a cidade de partida:");
+                    String cidade2 = s.nextLine().toUpperCase();
+
+                    System.out.println(cidade2 + cidade1);
+                    if (cidade1 == cidade2) {
+                        System.out.println("Você informou a mesma cidade!!!");
+                        break;
+                    }else {
+                        if (lc.verificaCidade(cidade1) && lc.verificaCidade(cidade2)) {
+                            System.out.println("As cidades selecionadas foram:");
+                            System.out.println("Partida:" + cidade1);
+                            System.out.println("Destino:" + cidade2);
+                            System.out.println("");
+                            System.out.println(" Escolha o tipo de caminhão para ser utilizado na rota acima!");
+                            System.out.println("1 - Pequeno");
+                            System.out.println("2 - Médio");
+                            System.out.println("3 - Grande");
+                            Caminhao caminhao = new Caminhao(s.nextInt());
+
+                            double distancia = lc.pegaDistancia(cidade1, cidade2);
+                            double valor = caminhao.Calculo(distancia, 1);
+
+                            System.out.println("Informações do trajeto:");
+                            System.out.println("Origem:" + cidade1);
+                            System.out.println("Destino:" + cidade2);
+                            System.out.println("Distância:" + distancia + "KM");
+                            System.out.println("Custo:" + valor);
+                            System.out.println("");
+                            System.out.println("");
+                            System.out.println("Aperte Enter para continuar");
+                            s.nextLine();
+                            s.nextLine();
+
+                        } else {
+                            System.out.println("Erro, tente novamente");
+                            System.out.println("Aperte Enter para continuar");
+                            s.nextLine();
+                            s.nextLine();
+
+                            break;
+                        }
+                    }
                     break;
                 case 2:
 
